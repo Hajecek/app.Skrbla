b3839f32a3e4909457f07f95bcd10576ad6b5f52
Hajecek <haj8cek@gmail.com>
2025-08-26 09:43:12 +0200

    Initial Commit

---
+++ Skrbla/SkrblaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkrblaApp.swift
+//  Skrbla
+//
+//  Created by Michal Hájek on 26.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct SkrblaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkrblaTests/SkrblaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkrblaTests.swift
+//  SkrblaTests
+//
+//  Created by Michal Hájek on 26.08.2025.
+//
+
+import Testing
+@testable import Skrbla
+
+struct SkrblaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
